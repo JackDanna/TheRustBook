f87280f41d28daf33d7b654cdd9f79667f5630d4
JackDanna <jackdanna78@gmail.com>
2024-06-14 15:12:20 -0400

    updated flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A Rust Dev Shell";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in 
+  {
+    # You can excute this command with "nix "
+    packages.${system} = {
+      default = pkgs.writeShellScriptBin "run" ''
+        nix develop -c -- codium .
+      '';
+    };
+    
+
+    devShells.${system}.default =
+      pkgs.mkShell rec {
+        name = "RustDevShell";
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          bashInteractive
+          (vscode-with-extensions.override  {
+            vscode = pkgs.vscodium;
+            vscodeExtensions = with pkgs.vscode-extensions; [
+              rust-lang.rust-analyzer
+              jnoortheen.nix-ide
+              mhutchie.git-graph
+              vscodevim.vim
+            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+              # {
+              #   name = "vscode-edit-csv";
+              #   publisher = "janisdd";
+              #   version = "0.8.2";
+              #   sha256 = "sha256-DbAGQnizAzvpITtPwG4BHflUwBUrmOWCO7hRDOr/YWQ=";
+              # }
+            ];
+          })
+
+        ];
+
+        shellHook = ''
+          export PS1+="${name}> "
+          echo "Welcome to the Rust Dev Shell"
+        '';
+      };
+  }; 
+
+}
+
