9d81831f30ac3b3fe97fbef0a0b4f507888288ed
Jack Danna <jackdanna78@gmail.com>
2024-10-06 19:42:25 -0400

    Added slice

---
+++ projects/slices/target/debug/deps/slices-d63007bf845700c8.d
@@ -0,0 +1,5 @@
+/home/jackd/repos/TheRustBook/projects/slices/target/debug/deps/libslices-d63007bf845700c8.rmeta: src/main.rs
+
+/home/jackd/repos/TheRustBook/projects/slices/target/debug/deps/slices-d63007bf845700c8.d: src/main.rs
+
+src/main.rs:
---
+++ projects/slices/target/debug/deps/slices-e19751457676764a.d
@@ -0,0 +1,5 @@
+/home/jackd/repos/TheRustBook/projects/slices/target/debug/deps/libslices-e19751457676764a.rmeta: src/main.rs
+
+/home/jackd/repos/TheRustBook/projects/slices/target/debug/deps/slices-e19751457676764a.d: src/main.rs
+
+src/main.rs:
